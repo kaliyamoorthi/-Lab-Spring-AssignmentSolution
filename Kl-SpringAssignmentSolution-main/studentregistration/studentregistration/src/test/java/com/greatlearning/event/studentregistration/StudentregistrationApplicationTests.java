a85fb13a52f8c14080bad60c9fa4ec96de606e2b
kaliyamoorthi <99474003+kaliyamoorthi@users.noreply.github.com>
2022-07-14 22:17:33 +0530

    Update StudentregistrationApplicationTests.java

--- Kl-SpringAssignmentSolution-main/studentregistration/studentregistration/src/test/java/com/greatlearning/event/studentregistration/StudentregistrationApplicationTests.java
+++ Kl-SpringAssignmentSolution-main/studentregistration/studentregistration/src/test/java/com/greatlearning/event/studentregistration/StudentregistrationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.greatlearning.event.studentregistration;
+package com.greatlearning.fest.studentregistration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
